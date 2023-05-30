35180c5a93869f11d7e2ab24ca0ee40a1dd01236
sightorld <vladvlasow2000@gmail.com>
2023-05-31 01:01:20 +0300

    chore: UIKit and Bender font added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    css: [
+        "@/node_modules/uikit/src/scss/uikit.scss",
+        "@/assets/fonts/bender.css"
+    ],
+    vite: {
+        css: {
+          preprocessorOptions: {
+            scss: {
+              additionalData: '@use "@/assets/styles/colors.scss" as *;@import "uikit/src/scss/variables-theme.scss"; @import "uikit/src/scss/mixins-theme.scss";',
+            },
+          },
+        }
+    }
 })
