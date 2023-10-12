8eb7c6c0d1f914f0a924b17d13611db3f90c49ed
sightorld <vladvlasow2000@gmail.com>
2023-10-12 12:21:47 +0300

    feat: error page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { NuxtError } from 'nuxt/app';
+
+interface Props {
+    error: NuxtError
+}
+defineProps<Props>()
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+    <NuxtLayout>
+        <AtomsTile :body="true" :top="true"  :tile-style="'margin-top: 20px;'" :body-style="'padding: 0 16px 12px'">
+            <template #top>
+                <h2 class="uk-text-primary">
+                    Something broke D:
+                </h2>
+            </template>
+            <template #body>
+                <NuxtLink @click.prevent.stop="handleError">
+                    {{ error.message + ' ! Go to main page..' }}
+                </NuxtLink>
+            </template>
+        </AtomsTile>
+    </NuxtLayout>
+</template>
